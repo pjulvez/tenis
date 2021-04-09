73f24724546fcd6b3ddbe5d7304d34c0880384a6
pjulvez <pjulvez@almata.cat>
2021-04-09 18:51:11 +0200

    implementacio del sprites

---
+++ tenis/js/player1.js
@@ -0,0 +1,15 @@
+class Player1 extends GameObject
+{
+  constructor(){
+    super(coordX,coordY);
+    this.coordX=coordX;
+    this.coordY=coordY;
+    this.direction=0;
+    this.lives = 3;
+    this.speed = 32;
+
+
+  }
+
+
+}
